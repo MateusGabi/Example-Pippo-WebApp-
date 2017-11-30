43fc9648be712552cce92d5fb97f837ee2882b9c
Mateus Gabi <mateusgabimoreira@hotmail.com>
2017-11-30 14:47:01 -0300

    tentativa 3
    Signed-off-by: Mateus Gabi <mateusgabimoreira@hotmail.com>

--- post-build.sh
+++ post-build.sh
@@ -5,14 +5,14 @@ echo "Post-building script starting..."
 echo "checking..."
 
 mvn -v
-gunzip -v
+unzip -v
 java -v
 
 echo "Maven packaging"
 mvn clean package
 
 echo "unzipping dist file"
-gunzip target/AppPippo-1.0.0.zip
+unzip target/AppPippo-1.0.0.zip -d dist
 
 echo "starting app"
-java -jar target/AppPippo-1.0.0/AppPippo-1.0.0.jar
+java -jar dist/AppPippo-1.0.0.jar
